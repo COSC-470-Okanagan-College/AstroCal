43af080683c560e9172814ec4e05cbff6c639624
Mitchell Vivian <mitchell.j.vivian@gmail.com>
2022-09-15 19:25:52 -0700

    Initial project structure

---
+++ AstroCal/__init__.py
@@ -0,0 +1,3 @@
+from core import run
+
+run()
---
+++ AstroCal/core.py
@@ -0,0 +1,19 @@
+
+def run():
+    hello()
+    fib(20)
+
+def hello():
+    print("Hello, moon!")
+
+def fib(n):
+    print(str(1))
+    next_fib(1, 0, 1, n)
+
+
+def next_fib(current, previous, i, limit):
+    i += 1
+    next = (current + previous)
+    print(str(next))
+    if (i < limit):
+        next_fib(next, current, i, limit)
