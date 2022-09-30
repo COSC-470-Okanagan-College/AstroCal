83d5c4a06e503f0cb57d22e3b630ddf51358e8f4
Logan-Campbell <loganbmcampbell@gmail.com>
2022-09-30 11:08:46 -0700

    Merge branch 'dev' of https://github.com/youry/AstroCal into dev

--- AstroCal/core.py
+++ AstroCal/core.py
@@ -11,6 +11,7 @@ def run():
     # print("Moon Rise and Set for Sept, 22")
     # print(control.getRiseSet(2022, 9, 22, 'MOON', 'RISE'))
     # print(control.getRiseSet(2022, 9, 22, 'MOON', 'SET'))
+    
     view.createMenu()
 
 def hello():
