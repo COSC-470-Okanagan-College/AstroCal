6d997f9649d94cb3c63e52227bf5b53463171b77
enoble450 <evan.noble@myokanagan.bc.ca>
2022-10-03 13:21:15 -0700

    commit

--- AstroCal/Calendar.py
+++ AstroCal/Calendar.py
@@ -5,18 +5,17 @@ from kivy.uix.gridlayout import GridLayout
 from datetime import datetime
 
 date = datetime.now()
-dm = date.month
 firstday = date.replace(day=1)
 startday = firstday.isoweekday()
 if startday == 7:
     startday = 0
 def daysInMonth():
-    if dm in [1,3,5,7,8,10,12]:
+    if date.month in [1,3,5,7,8,10,12]:
         return 31
-    elif dm in [4,6,9,11]:
+    elif date.month in [4,6,9,11]:
         return 30
-    elif dm == 2:
-        yr = datetime.now().year
+    elif date.month == 2:
+        yr = date.year
         if yr % 4 == 0 and (yr % 100 != 0 or yr % 400 == 0):
             return 29
         else:
