692c641478a9724f4aab98e1cf6366c409eb3e50
enoble450 <evan.noble@myokanagan.bc.ca>
2022-10-03 10:29:09 -0700

    commit

--- AstroCal/Calendar.py
+++ AstroCal/Calendar.py
@@ -1,9 +1,6 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-from kivy.core.window import Window
-from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.gridlayout import GridLayout
 from datetime import datetime
 
