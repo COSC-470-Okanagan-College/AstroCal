ee9c647172e7526af55ecab7793ddfac112f7a36
Fred <freddiebanfield@googlemail.com>
2022-10-02 19:07:36 -0700

    Run all tests from one file - Added test_All.py

--- tests/test_Eclipse.py
+++ tests/test_Eclipse.py
@@ -2,13 +2,12 @@ import unittest
 
 from AstroCal.control.control import getWhenSolEclipseLoc
 
+
 class TestEclipse(unittest.TestCase):
     def test_SolEcl_WhenSuccessful(self):
-        self.assertEqual(getWhenSolEclipseLoc(2022,9,27),(2023, 10, 14, 15, 9, 15.699218809604645),"Test Fail")
+        self.assertEqual(getWhenSolEclipseLoc(2022, 9, 27),
+                         (2023, 10, 14, 15, 9, 15.699218809604645), "Test Fail")
 
     def test_SolEcl_WhenUnsuccessful(self):
-        self.assertNotEqual(getWhenSolEclipseLoc(2022,9,27),(2024, 10, 14, 15, 9, 15.699218809604645),"Test Fail")
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertNotEqual(getWhenSolEclipseLoc(
+            2022, 9, 27), (2024, 10, 14, 15, 9, 15.699218809604645), "Test Fail")
