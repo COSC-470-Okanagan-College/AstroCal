0896ea75800756fe601be728b22d2ff412172801
Natasha Miles <nsmiles28@gmail.com>
2022-10-04 14:12:01 -0700

    Launch app from tk to kivy

--- AstroCal/core.py
+++ AstroCal/core.py
@@ -10,8 +10,8 @@ def run():
     # print("Moon Rise and Set for Sept, 22")
     # print(control.getRiseSet(2022, 9, 22, 'MOON', 'RISE'))
     # print(control.getRiseSet(2022, 9, 22, 'MOON', 'SET'))
-    app = view.tkGUI_Launch
-    app.mainloop()
+    app = view.home
+    app.run()
     #view.createMenu()
     #runs everything
     #app = view.mainApp()
