6478507540d9012fe5b74b547fb9d1e86aa3dc23
Fred <freddiebanfield@googlemail.com>
2022-10-04 14:30:58 -0700

    Fixed Test

--- tests/test_Eclipse.py
+++ tests/test_Eclipse.py
@@ -6,8 +6,8 @@ from AstroCal.control.control import getWhenSolEclipseLoc
 class TestEclipse(unittest.TestCase):
     def test_SolEcl_WhenSuccessful(self):
         self.assertEqual(getWhenSolEclipseLoc(2022, 9, 27),
-                         (2023, 10, 14, 15, 9, 15.699218809604645), "Test Fail")
+                         '2023-10-14 08:09:00-07:00', "Test Fail")
 
     def test_SolEcl_WhenUnsuccessful(self):
         self.assertNotEqual(getWhenSolEclipseLoc(
-            2022, 9, 27), (2024, 10, 14, 15, 9, 15.699218809604645), "Test Fail")
+            2024, 9, 27), '2023-10-14 08:09:00-07:00', "Test Fail")
