15f3ddf82d2effe1cbe304c3515740fc87622327
Michael W <mpwit2622@gmail.com>
2022-10-14 01:17:00 -0700

    I give up... for now....

--- AstroCal/view/Calendar_ui.py
+++ AstroCal/view/Calendar_ui.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from datetime import datetime
@@ -57,19 +58,17 @@ class CalendarApp(App):
     def getMonthName(self):
         return DATE.strftime("%B %Y")
 
-    def printToConsole(self):
-        print("Clicked a Calendar day")
+    def openDayView(self):
+        print("A day button was clicked.")
 
     def moveMonthBack(self):
         global DATE
         DATE = DATE + relativedelta(months=-1)
         print(DATE.strftime("%B %Y"))
-        #self.ids['month_label'].text = DATE.strftime("%B %Y")
-        #self.ids.recycleview_id.refresh_from_data()
+        self.month_label = DATE.strftime("%B %Y")
 
     def moveMonthForward(self):
         global DATE
         DATE = DATE + relativedelta(months=+1)
         print(DATE.strftime("%B %Y"))
-        #self.ids['month_label'].text = DATE.strftime("%B %Y")
-        #self.ids.recycleview_id.refresh_from_data()
+        self.month_label = DATE.strftime("%B %Y")
